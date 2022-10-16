5d2ef8c97b7d8bf0042bda4b5e03634cd85b364a
Harimad <leesh68481@gmail.com>
2022-10-16 17:00:33 +0900

    Add Item

--- todo/main.js
+++ todo/main.js
@@ -10,5 +10,33 @@ check 버튼을 누르면 할일이 끝나면서 밑줄이 간다
 진행중 끝남 탭을 누르면, 언더바가 이동
 끝남탭은, 끝난 아이템만, 진행중탭은 진행중인 아이템만
 전체탭을 누르면 다시 전체아이템으로 돌아옴
-
 */
+
+let taskInput = document.querySelector('#task-input');
+let addButton = document.querySelector('#add-button');
+let taskBoard = document.querySelector('#task-dashboard');
+let taskList = [];
+
+addButton.addEventListener('click', addTask);
+
+function addTask() {
+  let taskContent = taskInput.value;
+  taskList.push(taskContent);
+  render();
+}
+
+// taskList 그리는 함수
+function render() {
+  let resultHTML = '';
+  for (let i = 0; i < taskList.length; i++) {
+    resultHTML += `<div class="task">
+		<div>${taskList[i]}</div>
+		<div>
+			<button>Check</button>
+			<button>Delete</button>
+		</div>
+	</div>`;
+  }
+
+  taskBoard.innerHTML = resultHTML;
+}
