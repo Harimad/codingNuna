0738ca1ac6b59007779d63a3af5b91b55ed9cab6
Harimad <leesh68481@gmail.com>
2022-10-22 12:39:18 +0900

    clear

--- newApi/main.js
+++ newApi/main.js
@@ -1,18 +1,33 @@
-let news = [];
-const getLatestNews = async () => {
-  let url = new URL(
-    `https://api.newscatcherapi.com//v2/latest_headlines?countries=KR&topic=sport&page_size=10`
-  );
-  let header = new Headers({
-    'x-api-key': 'NHhmhsswltAdCYsRqKQaOZX_5XgKzCsxreKYRce6-Oo',
-  });
-  let response = await fetch(url, { headers: header });
-  let data = await response.json(); // 역시 서버에서 데이터를 뽑아야 하기때문에 await이 필요함.
+// 한달에 50번 다 써서 못씀.
+// let news = [];
+// const getLatestNews = async () => {
+// let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`);
+// let header = new Headers({
+//   'x-api-key': 'NHhmhsswltAdCYsRqKQaOZX_5XgKzCsxreKYRce6-Oo',
+// });
+// let response = await fetch(url, { headers: header });
+// let data = await response.json(); // 역시 서버에서 데이터를 뽑아야 하기때문에 await이 필요함.
+// news = data;
+// };
+// getLatestNews();
 
+let news = [];
+const getNews = async () => {
+  var url =
+    'https://newsapi.org/v2/everything?' +
+    'q=Apple&' +
+    'from=2022-10-21&' +
+    'sortBy=popularity&' +
+    'apiKey=2431d4c166fb4546a136e11ab9ca36ed';
+
+  let req = new Request(url);
+  let response = await fetch(req);
+  let data = await response.json();
   news = data.articles;
+  render();
   console.log(news);
 };
-// getLatestNews();
+getNews();
 
 /*
 1. 사이드 메뉴
@@ -28,6 +43,7 @@ const $search = document.querySelector('#search');
 const $closeBtn = document.querySelector('#closeBtn');
 const $menus = document.querySelector('#menus');
 const $searchArea = document.querySelector('#searchArea');
+const $articles = document.querySelector('#articles');
 
 $toggleMenu.addEventListener('click', e => {
   $menus.style.width = '250px';
@@ -60,4 +76,22 @@ $search.addEventListener('click', e => {
 // 3. 현재 console에서만 보이는 news 데이터들을 UI에 그려보기
 // - todo리스트에서 했던 render함수와 비슷함
 
-getLatestNews();
+let result = '';
+const render = () => {
+  // FOREACH (Map 써도됨)
+  news.forEach(item => {
+    result += `<div class="row news">
+  	<div class="col-lg-4">
+  		<img class="news-img" src="${item.urlToImage}" alt="">
+  	</div>
+  	<div class="col-lg-8">
+  		<h2>${item.title}</h2>
+  		<p>${item.description}</p>
+  		<div>
+  			${item.author} - ${item.publishedAt}
+  		</div>
+  	</div>
+  </div>`;
+  });
+  $articles.innerHTML = result;
+};
